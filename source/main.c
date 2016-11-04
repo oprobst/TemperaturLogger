66d9fdfa93d999d2e557e3ce0932288f38756ea7
Oliver Probst <OliverProbst@gmx.de>
2016-11-05 00:22:12 +0100

    First temperatur log implementation

--- source/main.c
+++ source/main.c
@@ -2,7 +2,7 @@
 
 #define F_CPU 1000000
 #define BAUD 4800
-
+#define MEASUREMENT_INTERVALL 2000 * 60 //every two minutes
 
 #include <stdint.h>
 #include <util/setbaud.h>
@@ -45,57 +45,77 @@ int main( void )
 	
 	uart_init();
 	
-	USART0_Transmit_String("\n\rDeleted EEPROM, wait for input \n\r");
-	USART0_Receive();
-
-	
-	for (int i = 0 ; i < 517; i++){
-		write_next_value(i/3);
-	}
- 
-    eeprom_reset_current_address ();
-    int nextValue = 0;
-    do{
-	  nextValue = read_next_value();
-	 	USART0_Transmit_String("\n\r");
-	  USART0_Transmit_Integer(nextValue);
-	} while (nextValue != -1);
-	
-	USART0_Transmit_String("End of EEPROM... \n\r");
-	USART0_Receive();
-	
-	//DDRA |= 0xFF;
-	DDRB |= ((1 << PB0) );
 	initADC();
 	
-	uint16_t supplyVolt = 0;
+	uint16_t supplyVolt = measure_supply();
 	
-	while(1)
-	{
-		PORTB ^= (1<<PB0);
-		uint16_t supplyVolt = measure_supply();
-		USART0_Transmit_String("\n\r");
-		USART0_Transmit_String("U=");
-		USART0_Transmit_Integer(supplyVolt);
-		USART0_Transmit_String("mV ");
-		
-		uint16_t temp = readTemperatur(supplyVolt);
-		USART0_Transmit_String(" T=");
-		
-		USART0_Transmit_Integer(temp);
-		 
-		temp = readInternalTemperatur(supplyVolt);
-		USART0_Transmit_String(" Ti=");
-		
-		USART0_Transmit_Integer(temp);
-		
-		
-		//USART0_Transmit_String(c);
-			 
-		//UART_Transmit_float(123.4f);
-		//USART0_Transmit(USART0_Receive()); /* Echo the received character */
-		//  USART0_Receive();
-		_delay_ms(1000);
+	if (supplyVolt < 4500){
+		while(1) {
+			
+			PORTB ^= (1<<PB0);
+			uint16_t temp = readTemperatur(measure_supply());
+			uint8_t write = (temp / 10.0);
+			write_next_value(write);
+			PORTB ^= (1<<PB0);
+			_delay_ms(MEASUREMENT_INTERVALL);
+			if (is_buffer_full()){
+				while(1)
+				{
+					_delay_ms(1000);
+					PORTB ^= (1<<PB0);
+					_delay_ms(20);
+					PORTB ^= (1<<PB0);
+				}
+			}
+			
+		}
+	} else{
+		for (int i = 0;  i < 10;i++){
+			_delay_ms(100);
+			PORTB ^= (1<<PB0);
+		}
+		while(1)
+		{
+			
+			USART0_Transmit_String("Ready for sending EEPROM! \n\r");
+			USART0_Receive();
+			int nextValue = 0;
+			do{
+				nextValue = read_next_value();
+				if (nextValue != -1){
+					USART0_Transmit_String("\n\r");
+					USART0_Transmit_Integer(nextValue);
+				}
+			} while (nextValue != -1);
+			
+			USART0_Transmit_String(" \n\r End of EEPROM... \n\r");
+			eeprom_reset_current_address();
+			USART0_Receive();
+			
+			/*
+			USART0_Transmit_String("\n\r");
+			USART0_Transmit_String("U=");
+			USART0_Transmit_Integer(supplyVolt);
+			USART0_Transmit_String("mV ");
+			
+			uint16_t temp = readTemperatur(supplyVolt);
+			USART0_Transmit_String(" T=");
+			
+			USART0_Transmit_Integer(temp);
+			
+			temp = readInternalTemperatur(supplyVolt);
+			USART0_Transmit_String(" Ti=");
+			
+			USART0_Transmit_Integer(temp);
+			*/
+			
+			//USART0_Transmit_String(c);
+			
+			//UART_Transmit_float(123.4f);
+			//USART0_Transmit(USART0_Receive()); /* Echo the received character */
+			//  USART0_Receive();
+			//_delay_ms(1000);
+		}
 	}
 }
 
@@ -103,8 +123,8 @@ int main( void )
 
 void USART0_Transmit_Integer(uint16_t value)
 {
-    char data[16];
-    sprintf(data, "%d", value);
+	char data[16];
+	sprintf(data, "%d", value);
 	USART0_Transmit_String(data);
 }
 
@@ -170,46 +190,46 @@ uint16_t readTemperatur (volatile uint16_t supplyVoltage) {
 	(0 << GSEL0);
 
 
-		ADMUXA =
-		(0 << MUX3)  |
-		(0 << MUX2)  |
-		(0 << MUX1)  |
-		(0 << MUX0);
-		
-		 uint16_t measure = readADCsamples(5);
-     return (((measure / 1023.0) * supplyVoltage / 1000.0) - 2.7315) * 1000.0;
+	ADMUXA =
+	(0 << MUX3)  |
+	(0 << MUX2)  |
+	(0 << MUX1)  |
+	(0 << MUX0);
+	
+	uint16_t measure = readADCsamples(5);
+	return (((measure / 1023.0) * supplyVoltage / 1000.0) - 2.7315) * 1000.0;
 }
-	
-	
-	
-	uint16_t readInternalTemperatur (volatile uint16_t supplyVoltage) {
-		ADMUXB=
-		(0 << REFS2) |
-		(0 << REFS1) |
-		(0 << REFS0) |
-		(0 << GSEL1) |
-		(0 << GSEL0);
 
 
-		ADMUXA =
-		(1 << MUX3)  |
-		(1 << MUX2)  |
-		(1 << MUX1)  |
-		(1 << MUX0);
-		
-		uint16_t measure = readADCsamples(5);
-		return (((measure / 1023.0) * supplyVoltage / 1000.0) - 2.7315) * 100.0;
-	}
+
+uint16_t readInternalTemperatur (volatile uint16_t supplyVoltage) {
+	ADMUXB=
+	(0 << REFS2) |
+	(0 << REFS1) |
+	(0 << REFS0) |
+	(0 << GSEL1) |
+	(0 << GSEL0);
+
+
+	ADMUXA =
+	(1 << MUX3)  |
+	(1 << MUX2)  |
+	(1 << MUX1)  |
+	(1 << MUX0);
 	
-	
+	uint16_t measure = readADCsamples(5);
+	return (((measure / 1023.0) * supplyVoltage / 1000.0) - 2.7315) * 100.0;
+}
+
+
 uint16_t readADC (volatile uint8_t channel) {
-		ADMUXB=
-		(0 << REFS2) |
-		(0 << REFS1) |
-		(0 << REFS0) |
-		(0 << GSEL1) |
-		(0 << GSEL0);
-		
+	ADMUXB=
+	(0 << REFS2) |
+	(0 << REFS1) |
+	(0 << REFS0) |
+	(0 << GSEL1) |
+	(0 << GSEL0);
+	
 	switch (channel){
 		case 0: //Temp Sensor
 		ADMUXA =
