eb80ddbb4bb663f07d641e69c092c5d550edafe4
Oliver Probst <OliverProbst@gmx.de>
2016-11-04 23:19:40 +0100

    Added some additional comments

--- source/eeprom-store.h
+++ source/eeprom-store.h
@@ -1,5 +1,3 @@
-
-
 #include <stdint.h>
 
 #ifndef EEPROM_STORE_H
@@ -15,13 +13,24 @@
 #define FALSE 0
 #endif
 
-
+/*
+ * Write a value to the next free byte in eeprom
+ */
 void write_next_value (uint8_t value);
 
+/*
+ * Read next byte in eeprom
+ */
 int read_next_value (void);
 
+/*
+ * Return 1 if read or write came to the end of eeprom or last byte in read mode
+ */
 uint8_t is_buffer_full (void);
 
+/*
+ * reset pointer to the begining of eeprom, usually only for testing purposes.
+ */
 void eeprom_reset_current_address (void);
 
 #endif
